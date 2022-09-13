37bd15651593d57a706eefcfd2ff936639afcd87
evgeniy <admiralchopchop@gmail.com>
2022-09-14 02:37:41 +0300

    finish

--- build/js/map-script.js
+++
@@ -1,3 +0,0 @@
-let map = document.querySelector('.contacts__map-container');
-
-map.classList.remove('contacts__map-container--nojs');
--- build/js/modal-script.js
+++
@@ -1,6 +0,0 @@
-let orederButton = document.querySelector('.product-of-the-week__order');
-let modal = document.querySelector('.modal-container');
-
-orederButton.addEventListener('click', function () {
-  modal.classList.remove('modal-container--closed');
-});
