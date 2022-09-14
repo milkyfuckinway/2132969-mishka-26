eecdb68908f7f3b0dcae17ffd9c4e0e8374e01f0
evgeniy <admiralchopchop@gmail.com>
2022-09-14 18:35:09 +0300

    minor fixes manifest

--- source/js/video-script.js
+++
@@ -1,8 +0,0 @@
-let demonstration = document.querySelector('.demonstration__video');
-let videoButton = document.querySelector('.demonstration__play');
-
-videoButton.addEventListener('click', function () {
-    videoButton.classList.add('demonstration__play--video-started');
-    demonstration.classList.add('demonstration__video--started');
-  }
-);
