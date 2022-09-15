055e4e53a26c95d06abb7fc673e2c0ddaea5a116
evgeniy <admiralchopchop@gmail.com>
2022-09-15 23:35:37 +0300

    fixes

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,5 @@
 let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__menu-toggle');
+let navToggle = document.querySelector('.main-nav__menu-button');
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--closed');
