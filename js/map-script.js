60a88813ce187875d6e82073fe0362f11624a7e1
milkyfuckinway <0-0_0@bk.ru>
2023-03-13 09:13:43 +0300

    Updates

---
+++ js/map-script.js
@@ -0,0 +1,1 @@
+let map=document.querySelector(".contacts__map-container");map.classList.remove("contacts__map-container--nojs");
---
+++ js/modal-script.js
@@ -0,0 +1,1 @@
+let orederButton=document.querySelector(".product-of-the-week__order"),modal=document.querySelector(".modal-container");orederButton.addEventListener("click",(function(){modal.classList.remove("modal-container--closed")}));
